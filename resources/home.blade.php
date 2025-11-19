7f2a60f363a433bd25087757ba573e8dce7d21c5
Dean Naufal <deannaufal2909@gmail.com>
2025-11-19 14:01:01 +0700

    Home blade

---
+++ resources/home.blade.php
@@ -0,0 +1,22 @@
+{{-- File: resources/views/home.blade.php --}}
+
+@extends('layouts.app') {{-- Menggunakan master layout --}}
+
+@section('title', 'Home - Rumah Que-Que')
+
+@section('content')
+    {{-- Konten Hero Section --}}
+    <section class="hero-section container d-flex align-items-center py-0" style="margin-top:0;">
+        {{-- ... Pindahkan semua HTML Hero dari HomeUtama.php ke sini ... --}}
+    </section>
+
+    {{-- Konten Produk Terlaris --}}
+    <div class="produk-terlaris-box-oren">
+        {{-- ... Pindahkan semua HTML Produk Terlaris dari HomeUtama.php ke sini ... --}}
+    </div>
+
+    {{-- Konten FAQ --}}
+    <section class="faq-section py-5">
+        {{-- ... Pindahkan semua HTML FAQ dari HomeUtama.php ke sini ... --}}
+    </section>
+@endsection
