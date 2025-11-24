3bbffd8ad299210283357e2d6429b0b27941e08c
Dean Naufal <deannaufal2909@gmail.com>
2025-11-24 09:44:11 +0700

    hapus file auth google n hash

--- hash_generator.php
+++
@@ -1,6 +0,0 @@
-<?php
-$password_plain = '12345';
-// Gunakan algoritma hashing yang kuat
-$hashed_password = password_hash($password_plain, PASSWORD_DEFAULT);
-echo "Password Hashed: " . $hashed_password;
-?>
